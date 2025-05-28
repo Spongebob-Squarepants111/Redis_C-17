5aa6b736f7351fa5771fc39571c080068353b9ad
Spongebob-Squarepants111 <your.email@example.com>
2025-05-28 12:43:31 +0800

    本åmodify README.md

--- test_eff.sh
+++ test_eff.sh
@@ -1,3 +1,4 @@
 redis-benchmark -h 127.0.0.1 -p 6379 -t set -n 1000000 -c 50
 redis-benchmark -h 127.0.0.1 -p 6379 -t get -n 1000000 -c 50
 redis-benchmark -h 127.0.0.1 -p 6379 -t set -n 1000000 -c 50 -P 16
+redis-benchmark -h 127.0.0.1 -p 6379 -t set -n 1000000 -c 10000
